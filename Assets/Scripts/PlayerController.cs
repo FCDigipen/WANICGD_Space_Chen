53ac11ea3c656c9856dcc4d40929009c3052a745
FCDigipen <franklin.chen@digipen.edu>
2025-07-07 09:32:58 -0700

    removed debug logs and made onclick set player velocity to function properly, and also set recoil to non-zero value

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -34,19 +34,19 @@ public class PlayerController : MonoBehaviour
 
     // called whenever the mouse is clciekd
     void OnClick(Vector2 mousePos) {
-        Debug.Log(mousePos);
+        Vector2 dir = Rotate(mousePos);
+        playerVelocity = -dir * recoil;
     }
 
     // called whenever the mouse is moved
     void OnMove(Vector2 mousePos) {
-        Debug.Log(mousePos);
         Rotate(mousePos);
     }
 
     // rotate player dependent on mousePos. returns direction vector for OnMove
     private Vector2 Rotate(Vector2 mousePos) {
         Vector2 dir = (mousePos - (Vector2) transform.position).normalized;
-        float angle = Vector2.Angle(dir, (Vector2) transform.right); // base angle
+        float angle = Vector2.Angle(dir, (Vector2) transform.right); // d(angle)
         transform.Rotate(Vector3.forward * angle);
         return dir;
     }
