3a3c36c7797c975c7ccb3f771cb2880bd0febd78
FCDigipen <franklin.chen@digipen.edu>
2025-07-07 08:11:09 -0700

    added OnMove and OnClick functions that work with the old input system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Camera cam;
+    
+    private Vector2 lastMousePos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // mouse position in world space
+        if(mousePos != lastMousePos) {OnMove(mousePos);}
+        if(Input.GetMouseButtonDown(0)) {OnClick(mousePos);}
+    }
+
+    // called whenever the mouse is clciekd
+    void OnClick(Vector2 mousePos) {
+        Debug.Log("Mouse Clicked");
+    }
+
+    // called whenever the mouse is moved
+    void OnMove(Vector2 mousePos) {
+        Debug.Log("Mouse Moved");
     }
 }
