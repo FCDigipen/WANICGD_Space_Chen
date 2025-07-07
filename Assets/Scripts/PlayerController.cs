ad37f0caef7ae38dc1a9ea0c266f796c84697885
FCDigipen <franklin.chen@digipen.edu>
2025-07-07 08:55:25 -0700

    made possibly disfunct rotate function to rotate player (once sprite is made)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,10 +25,20 @@ public class PlayerController : MonoBehaviour
     // called whenever the mouse is clciekd
     void OnClick(Vector2 mousePos) {
         Debug.Log("Mouse Clicked");
+        Rotate(mousePos);
     }
 
     // called whenever the mouse is moved
     void OnMove(Vector2 mousePos) {
         Debug.Log("Mouse Moved");
+        Vector2 dir = Rotate(mousePos);
+    }
+
+    // rotate player dependent on mousePos. returns direction vector for OnMove
+    private Vector2 Rotate(Vector2 mousePos) {
+        Vector2 dir = (mousePos - (Vector2) transform.position).normalized;
+        float angle = Vector2.Angle(dir, new Vector2(1f,0f)); // base angle
+        transform.Rotate(Vector3.forward * angle);
+        return dir;
     }
 }
