3a0fb80682cab0fb2fef9e23a31651c2d00f569b
FCDigipen <franklin.chen@digipen.edu>
2025-07-07 07:59:57 -0700

    reverted input system code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,9 +14,5 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
-    }
-
-    public void OnClick(InputAction.CallbackContext clickContext) {
-        Debug.Log(clickContext.ReadValue<Vector3>());
     }
 }
