9164638cfc2056af22f64beefe838a0785378467
FCDigipen <franklin.chen@digipen.edu>
2025-07-07 08:37:48 -0700

    removed old input system code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -11,15 +14,13 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // mouse position in world space
-        if(mousePos != lastMousePos) {OnMove(mousePos); lastMousePos = mousePos;}
-        if(Input.GetMouseButtonDown(0)) {OnClick(mousePos);}
+
     }
 
     // called whenever the mouse is clciekd
