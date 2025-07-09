fba4e84f4c3e87f49faa584c4512170d4151f33a
FCDigipen <franklin.chen@digipen.edu>
2025-07-09 11:09:04 -0700

    Revert "added randomized rotate time to enemy"
    This reverts commit 846b9ebb539393a247be300e622d0151471826d6.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +6,6 @@ public class EnemyController : MonoBehaviour
 {
     [SerializeField] private int health;
     [SerializeField] private GameObject explosionVFX;
-    [Tooltip("time between retreiving player location and rotating to it")]
-    [SerializeField] private float rotateTime;
-    [Tooltip("slight randomized time between time queries")]
-    [SerializeField] private float randomRotateTime;
     private GameObject player;
     private List<ParticleSystem> particles = new List<ParticleSystem>();
 
@@ -23,19 +18,17 @@ public class EnemyController : MonoBehaviour
             ParticleSystem ps = explosionVFX.transform.GetChild(i).GetComponent<ParticleSystem>();
             if(ps) {particles.Add(ps);}
         }
-
-        StartCoroutine(DelayedRotate());
     }
 
-    private IEnumerator DelayedRotate() {
-        yield return new WaitForSeconds(rotateTime + UnityEngine.Random.Range(0f, 1f) * randomRotateTime);
+    // Update is called once per frame
+    void Update()
+    {
         Rotate(player.transform.position);
-        StartCoroutine(DelayedRotate());
-    } 
+    }
 
     // rotate player dependent on targetPos
-    private void Rotate(Vector2 playerPos) {
-        Vector2 dir = (playerPos - (Vector2) transform.position).normalized;
+    private void Rotate(Vector2 mousePos) {
+        Vector2 dir = (mousePos - (Vector2) transform.position).normalized;
         Quaternion qdir = Quaternion.LookRotation(dir, Vector3.right);
         transform.rotation = qdir * Quaternion.Euler(0,-90,0);
     }
