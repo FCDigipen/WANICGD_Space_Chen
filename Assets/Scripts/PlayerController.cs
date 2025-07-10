5fcce923f0b354d8aefec0179dd756bb1c86fa76
FCDigipen <franklin.chen@digipen.edu>
2025-07-10 11:23:55 -0700

    fixed playerController.cs class name from gun to playerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Animations;
 
-public class Gun : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Camera cam;
