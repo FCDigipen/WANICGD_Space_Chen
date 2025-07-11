a691b39d2d272b2a2f90c86ddaeb7337fd25a13e
FCDigipen <franklin.chen@digipen.edu>
2025-07-11 09:28:11 -0700

    created ShotCounter.cs and added to renamed shotCounter object (previously ammo text)

---
+++ Assets/Scripts/ShotCounter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShotCounter : MonoBehaviour
+{
+    // manage shots
+    private int shots = 0;
+    private TextMeshProUGUI text;
+
+    public int getShots {get => shots;}
+
+    // Start is called before the first frame update
+    void Start(){text = GetComponent<TextMeshProUGUI>();}
+    void UpdateShots(){++shots; text.text = $"shots={shots}";}
+
+}
